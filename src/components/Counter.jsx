4a3588ec2769beaea1819c3d95c42ec1ff6aa9c8
wandakim <nwd1583@naver.com>
2023-04-06 23:19:32 +0900

    useState Count

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,7 +8,9 @@ export default function Counter() {
       <button
         className='button'
         onClick={() => {
-          setCount(count + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
         }}
       >
         Add +
