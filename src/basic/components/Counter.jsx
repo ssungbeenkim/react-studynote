b62d674bcc05ae0da5ba075b083632a711d3ab87
wandakim <nwd1583@naver.com>
2023-05-08 21:03:55 +0900

    Review: state update with callback

--- src/basic/components/Counter.jsx
+++ src/basic/components/Counter.jsx
@@ -11,7 +11,10 @@ export default function Counter({ total, onClick }) {
       <button
         className='button'
         onClick={() => {
-          setCount((prev) => prev + 1);
+          // setCount(count + 1);
+          // setCount(count + 1);
+          // setCount(count + 1); // 이렇게 여러번 해봤자 1씩 증가한다. 클로저와 관련.
+          setCount((prev) => prev + 1); // 이전 상태값을 받아서 처리하는 함수를 넣어주면 된다.
           onClick();
         }}
       >
