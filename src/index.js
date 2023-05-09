717293a6c884b70dfc263e302c1a0342ecf15b68
wandakim <nwd1583@naver.com>
2023-05-09 16:23:47 +0900

    Review: Cutom Hooks

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppMemoCallback from './AppMemoCallback';
+import AppCustomHook from './AppCustomHook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMemoCallback />
+    <AppCustomHook />
   </React.StrictMode>
 );
 
